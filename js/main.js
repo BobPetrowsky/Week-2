c565cbb7cc80759bd3bb044cca060dfa392f0c3e
Robert Petrowsky <Bob@Roberts-MacBook-Air.local>
2015-11-07 22:21:19 -0500

    added requirejs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+requirejs.config({
+    'baseUrl': 'js/',
+    'paths': {
+      'jquery': 'vendors/jquery.min',
+      'bootstrap': 'vendors/bootstrap.min'
+    },
+    shim: {
+      'bootstrap': ['jquery']
+    }
+});
+
 $(function(){
 
   var playlist = new Playlist();
